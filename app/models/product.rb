d824e7313b84367f85bb8643791088db3ae2cbcb
William May <williammayy@gmail.com>
2025-10-28 21:44:03 -0300

    adding cart items to connect cart and product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   validates_presence_of :name, :price
   validates_numericality_of :price, greater_than_or_equal_to: 0
+  has_many :carts, through: :cart_items
 end
